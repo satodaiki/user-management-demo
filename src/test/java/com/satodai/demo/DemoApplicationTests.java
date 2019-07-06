c123429eac2b8ce1072a5da6cf7909959b2e52d1
banana-chan <banana5388@gmail.com>
2019-07-06 12:42:18 +0900

    first commit.

---
+++ src/test/java/com/satodai/demo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.satodai.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

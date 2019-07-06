eede1f77519564f2d2940ca0663cca2df6228541
banana-chan <banana5388@gmail.com>
2019-07-06 14:26:41 +0900

    feature: ログイン関係のコントローラ実装

---
+++ src/main/java/com/satodai/demo/login/controller/SignupController.java
@@ -0,0 +1,39 @@
+package com.satodai.demo.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Controller
+public class SignupController {
+
+    private Map<String, String> radioMarriage;
+
+    private Map<String, String> initRadioMarriage() {
+        Map<String, String> radio = new LinkedHashMap<>();
+
+        radio.put("既婚", "true");
+        radio.put("未婚", "false");
+
+        return radio;
+    }
+
+    @GetMapping("signup")
+    public String getSignUp(Model model) {
+
+        radioMarriage = initRadioMarriage();
+
+        model.addAttribute("radioMarriage", radioMarriage);
+
+        return "login/signup";
+    }
+
+    @PostMapping("signup")
+    public String postSignUp(Model model) {
+        return "redirect:/login";
+    }
+}
